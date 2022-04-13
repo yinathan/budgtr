f832a045c2f39192870dca9b6a6f476891c5de8c
Nathan Yi <nathanyi112@gmail.com>
2022-04-13 14:10:22 -0500

    budgtr first draft complete

--- models/budget.js
+++ models/budget.js
@@ -1,4 +1,4 @@
-module.exports = [
+budget = [
   {
     date: 'April 1',
     name: 'Income',
--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 const express = require("express");
 const app = express()
 const budget = require('./models/budget.js')
+const bodyParser = require("body-parser");
 
+
+app.use(express.static('./public/'));
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false}))
 
 //index
 app.get('/budgets', (req, res) => {
-    res.send('Hello')
-})
-
-//show   GET  /budgets/:index
-app.get('/budgets/:index', (req, res) => {
-    res.send('Hello again')
+    res.render('index.ejs', {allBudget: budget})
 })
 
 //new    GET /budgets/new
 app.get('/budgets/new', (req, res) => {
-    res.send('Hello a third time')
+    res.render('new.ejs', {budget: budget})
 })
+
+//show   GET  /budgets/:index
+app.get('/budgets/:id', (req, res) => {
+    res.render('show.ejs', {allBudget: budget[req.params.id]})
+})
+
+
+
 //create  POST /budgets
-app.post('/orders', (req, res) => {
-    res.send('something something post')
+app.post('/budgets', (req, res) => {
+    budget.push(req.body);
+    res.redirect("/budgets")
 })
 
 app.listen(3000, () => {
